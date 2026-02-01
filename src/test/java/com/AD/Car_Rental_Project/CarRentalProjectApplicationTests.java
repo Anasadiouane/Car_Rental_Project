6661dcbcbe2135d5181f4d779a83e7b524c91bb2
anasadiouane <anasadiouane5@gmail.com>
2026-02-01 23:42:00 +0100

    initial commit

---
+++ src/test/java/com/AD/Car_Rental_Project/CarRentalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AD.Car_Rental_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
